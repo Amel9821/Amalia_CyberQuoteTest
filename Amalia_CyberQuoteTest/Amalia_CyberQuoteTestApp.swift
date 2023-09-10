daf72a9fc4556fd985ad7722a235a8c8f2fa990a
Amel9821 <amalia21198@gmail.com>
2023-09-10 20:43:44 +0700

    Initial Commit

---
+++ Amalia_CyberQuoteTest/Amalia_CyberQuoteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amalia_CyberQuoteTestApp.swift
+//  Amalia_CyberQuoteTest
+//
+//  Created by Amalia . on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Amalia_CyberQuoteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
